c2c8b55605fadf723df111f3f142a3caebda955c
ali.jahanpak <ali.jahanpak@gmail.com>
2019-08-08 10:38:19 +0430

    first commit for push in repo 2019/08/08

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,46 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <autoComplete_textArea
+            style="height: 150px;"
+            :name="'description'"
+            :options = "arrayOption"
+            v-model="description">
+    </autoComplete_textArea>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import autoComplete_textArea from './components/autoComplete-textArea.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'app',
+        components: {
+            autoComplete_textArea
+        },
+        data () {
+            return {
+                arrayOption : [
+                    {id : 1 , text : 'hello, my name is Ali'},
+                    {id : 2 , text : 'hello, my name is Morteza'},
+                    {id : 3 , text : 'hello, my name is Morteza'},
+                    {id : 4 , text : 'hello, my name is Morteza'},
+                    {id : 5 , text : 'hello, my name is Morteza'},
+                    {id : 6 , text : 'hello, my name is Morteza'},
+                    {id : 7 , text : 'hello, my name is Morteza'},
+                    {id : 8 , text : 'hello, my name is Morteza'},
+                    {id : 9 , text : 'hello, my name is Morteza'},
+                    {id : 10, text : 'hello, my name is Morteza'},
+                    {id : 11, text : 'hello, my name is Morteza'},
+                    {id : 12, text : 'hello, my name is Morteza'},
+                    {id : 13, text : 'hello, my name is Morteza'},
+                    {id : 14, text : 'hello, my name is Morteza'},
+                    {id : 15, text : 'hello, my name is Morteza'},
+                    {id : 16, text : 'hello, my name is Morteza'},
+                    {id : 17, text : 'hello, my name is Morteza'},
+                    {id : 18, text : 'hello, my name is Morteza'},
+                ],
+                description : '',
+            }
+        },
+    }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import jQuery from 'jQuery'
+window.jQuery = jQuery
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+global.jQuery = require('jQuery');
+var $ = global.jQuery;
+window.$ = $;
